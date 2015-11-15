99e92d67fb7ee712c101b4b55c704c5f913fac30
Maxim Therrien <acerspyro@gmail.com>
2015-11-15 15:14:07 -0500

    Mobile morphing, added dialogs with contact info and about, still gotta fix cookies though.

--- lang/en.js
+++ lang/en.js
@@ -1,11 +1,15 @@
 // ENGLISH LANGUAGE FILE
 
+var _ABOUT_MSG = "I made this site by hand using nothing but Gedit and SCSS, using the latest HTML5, CSS3 and ECMAscript 6 technologies. Latest revision was on November 15, 2015. Copyright &copy; Maxim \"Acerspyro\" Therrien, all rights reserved.",
+	_CONTACT_MSG = "If you desire to contact me, please do so via e-mail first at:<br/><br/>acerspyro (at) gmail (dot) com";
+
 langres = {
 	"_CODEPEN_BUTTON": "Codepen",
 	"_GITHUB_BUTTON": "GitHub",
 	"_CONTACT_BUTTON": "Contact",
 	"_ABOUT_BUTTON": "About",
 	"_LANG_BUTTON": "Français",
+	"_CLOSE_BUTTON": "Close",
 	"_TOP_TEXT": "<h1>Acerspyro, checking in.</h1>\
 			<h4>- Said I.</h4>",
 	"_SECTION_BACKSTORY": "<h3>Backstory? Backstory.</h3>\
@@ -13,11 +17,11 @@ langres = {
 						Hello there, my name is Maxim Therrien, known as Acerspyro around the Internet.\
 					</p>\
 					<p>\
-						I am a 17 year old computer enthusiast who enjoys finding new ways to use technology to make cool new stuff. I am also a collegian on his way of getting a DCS in software development at the Édouard Montpetit CÉGEP. French is my native tongue (I'm a Québécois, after all), but as you can see, I can speak English just as well (Note: If you want to read this site in French, there's a button in the topbar).\
+						I am a 17 year old computer enthusiast who enjoys finding new ways to use technology to make cool new stuff. I am also a collegian on his way of getting a DCS in software development at the Édouard Montpetit CÉGEP. French is my native tongue (I'm a Québécois, after all), but as you can see, I can speak English just as well (note: If you want to read this site in French, there's a button in the topbar).\
 					</p>",
 	"_SECTION_SKILLS": "<h3>Skills</h3>\
 					<p>\
-						To be fair, even though I have not really worked anywhere (yet, and I'd love to work somewhere, believe me), I did contribute to a few open source projects (See the section called \"A small Introduction\"), but I also have a month-long internship experience in the IT domain at an accounting firm known as Fuller Landau. They are very nice people and I really enjoyed the experience, because for once, I felt useful. This is really all I need to be productive: make me useful, and I will work relentlessly.\
+						To be fair, even though I have not really worked anywhere (yet, and I'd love to work somewhere, believe me), I did contribute to a few open source projects (see the section called \"A small Introduction\"), but I also have a month-long internship experience in the IT domain at an accounting firm known as Fuller Landau. They are very nice people and I really enjoyed the experience, because for once, I felt useful. This is really all I need to be productive: make me useful, and I will work relentlessly.\
 					</p>",
 	"_SECTION_INTRO": "<h3>A small Introduction</h3>\
 					<p>\
@@ -27,7 +31,7 @@ langres = {
 						I usually like to stick around on Freenode's IRC, often in #web, helping people with their webdev stuff. I love making cool, nifty stuff on Codepen when I get bored. You should check it out, it's all showcased later on this page!\
 					</p>\
 					<p>\
-						Anyways, I have contributed to quite a few open source projects on GitHub and I am part of the Neothyne team (You may not know what that is, I do not blame you). I used to be part of the Red Eclipse team as well, I mostly contributed in scripts and art assets, but also worked as a team with other people to make things work.\
+						Anyways, I have contributed to quite a few open source projects on GitHub and I am part of the Neothyne team (you may not know what that is, I do not blame you). I used to be part of the Red Eclipse team as well, I mostly contributed in scripts and art assets, but also worked as a team with other people to make things work.\
 					</p>",
 	"_SECTION_PROJECTS": "<h3>Projects</h3>\
 					<ul>\
--- main.js
+++ main.js
@@ -1,12 +1,52 @@
+/* TODO:
+ *	- Add a way to change language without cookies
+ *  - BUG: Cookies not working in Chrome
+ *
+ */
 var lang;
 
 function changeLang() {
 	if (lang=='en')
-		document.cookie='lang=fr';
+		document.cookie='lang=fr;expires=999999999999999999';
 	else
-		document.cookie='lang=en';
+		document.cookie='lang=en;expires=999999999999999999';
 	location.reload();
 }
+
+function openContactDialog() {
+	if (document.getElementsByClassName("messagebox")[0].className.search('contact') == -1) {
+		if (document.getElementsByClassName("messagebox")[0].className.search('open') == -1) {
+			document.getElementById("_MESSAGE").innerHTML = _CONTACT_MSG;
+			document.getElementsByClassName("messagebox")[0].className = "messagebox open contact";
+		} else {
+			document.getElementsByClassName("messagebox")[0].className = "messagebox";
+			setTimeout(function(){
+				document.getElementById("_MESSAGE").innerHTML = _CONTACT_MSG;
+				document.getElementsByClassName("messagebox")[0].className = "messagebox open contact";
+			}, 250);
+		}
+	}
+}
+
+function openAboutDialog() {
+	if (document.getElementsByClassName("messagebox")[0].className.search('about') == -1) {
+		if (document.getElementsByClassName("messagebox")[0].className.search('open') == -1) {
+			document.getElementById("_MESSAGE").innerHTML = _ABOUT_MSG;
+			document.getElementsByClassName("messagebox")[0].className = "messagebox open about";
+		} else {
+			document.getElementsByClassName("messagebox")[0].className = "messagebox";
+			setTimeout(function(){
+				document.getElementById("_MESSAGE").innerHTML = _ABOUT_MSG;
+				document.getElementsByClassName("messagebox")[0].className = "messagebox open about";
+			}, 250);
+		}
+	}
+}
+
+function closeDialog() {
+	document.getElementsByClassName("messagebox")[0].className = "messagebox";
+}
+
 function init() {
 	var c=document.cookie.split(';');
 	for (i=0;i<c.length;i++) {
@@ -28,4 +68,4 @@ function init() {
 	document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.onload = init();
